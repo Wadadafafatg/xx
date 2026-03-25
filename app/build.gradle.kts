9c11575c97dd1ca80d238934a485896744f90e9b
Wadadafafatg <mnnnst93@gmail.com>
2026-03-25 03:02:31 +0300

    Upgrade compileSdk and targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.videoplayer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.videoplayer"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
